312b2688565fa492f8cacfd63440c7f835f6bb4b
Beau Hoyt <beau.g.hoyt@gmail.com>
2026-02-16 14:34:54 -0800

    Some Refactoring And Tesing/Benchmarking Alt Funcs
    * Created two other getMultiplierByUnitString() functions with different
      designs to test if we could get better that compiler. Results pretty
      much point to that compiler is very very good.
    * Added getMultiplierByUnitStringNestedSwitchesVersion() to test if multi
      test switch branches was faster than what the compiler came up with just
      one switch statement. Was very very close but 1~3ns slower than the
      original.
    * Added getMultiplierByUnitStringMapVersion() to test to see if a hash
      map lookup with all the possible answer faster than a switch
      statement. It't not it was the slowest.
    * Added Benchmarks for these tests in bytesize_test.go
    * Upgrade to 1.26.0 in go.mod
    * Added tool gocyclo
    * Added to Makefile a directive for cyclo and for bencmarking
      getMultiplierByUnitString alternative functions
    * Modify Makefile fuzz test statement to be more flexible
    * Add .gitignore file

---
+++ benchmark_other_function_designs.go
@@ -0,0 +1,251 @@
+package bytesize
+
+import (
+	"fmt"
+	"strings"
+)
+
+// UnitStringToBytes maps valid unit strings to their corresponding Bytes
+// multiplier values for parsing.
+var UnitStringToBytes = map[string]Bytes{
+	"b":  B,
+	"kb": KB, "mb": MB, "gb": GB, "tb": TB, "pb": PB, "eb": EB, "zb": ZB, "yb": YB, "rb": RB, "qb": QB,
+	"kib": KiB, "mib": MiB, "gib": GiB, "tib": TiB, "pib": PiB, "eib": EiB, "zib": ZiB, "yib": YiB, "rib": RiB, "qib": QiB,
+	"byte": B, "bytes": B,
+	"kilobyte": KB, "kilobytes": KB, "megabyte": MB, "megabytes": MB, "gigabyte": GB, "gigabytes": GB,
+	"terabyte": TB, "terabytes": TB, "petabyte": PB, "petabytes": PB, "exabyte": EB, "exabytes": EB, "zettabyte": ZB, "zettabytes": ZB, "yottabyte": YB, "yottabytes": YB,
+	"ronnabyte": RB, "ronnabytes": RB, "quettabyte": QB, "quettabytes": QB,
+	"kibibyte": KiB, "kibibytes": KiB, "mebibyte": MiB, "mebibytes": MiB, "gibibyte": GiB, "gibibytes": GiB,
+	"tebibyte": TiB, "tebibytes": TiB, "pebibyte": PiB, "pebibytes": PiB, "exbibyte": EiB, "exbibytes": EiB,
+	"zebibyte": ZiB, "zebibytes": ZiB, "yobibyte": YiB, "yobibytes": YiB, "ronnibyte": RiB, "ronnibytes": RiB, "quettibyte": QiB, "quettibytes": QiB,
+}
+
+// getMultiplierForUnit returns the multiplier Bytes value corresponding to the
+// given unit string. It looks up the unit string in the UnitStringToBytes map
+// and returns the corresponding multiplier, or an error if the unit is unknown.
+func getMultiplierByUnitStringMapVersion(unitStr string) (Bytes, error) {
+	unitStr = strings.ToLower(strings.TrimSpace(unitStr))
+	multiplier, found := UnitStringToBytes[unitStr]
+	if !found {
+		return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+	}
+	return multiplier, nil
+}
+
+// getMultiplierByUnitStringNestedSwitchesVersion is an alternative implementation of
+// getMultiplierByUnitString that uses a switch statement to determine the multiplier
+// based on the first few characters of the unit string. This approach may be more
+// efficient than a map lookup for a small number of units, but it is less flexible
+// and harder to maintain if new units are added in the future.
+func getMultiplierByUnitStringNestedSwitchesVersion(unitStr string) (Bytes, error) {
+	unitStr = strings.ToLower(strings.TrimSpace(unitStr))
+	switch unitStr[0] {
+	case 'b':
+		return B, nil
+	case 'k':
+		switch unitStr[1] {
+		case 'b':
+			return KB, nil
+		case 'i':
+			switch unitStr[2] {
+			case 'b':
+				return KiB, nil
+			case 'l':
+				return KB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'm':
+		switch unitStr[1] {
+		case 'b':
+			return MB, nil
+		case 'i':
+			return MiB, nil
+		case 'e':
+			switch unitStr[2] {
+			case 'b':
+				return MiB, nil
+			case 'g':
+				return MB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'g':
+		switch unitStr[1] {
+		case 'b':
+			return GB, nil
+		case 'i':
+			switch unitStr[2] {
+			case 'b':
+				return GiB, nil
+			case 'g':
+				return GB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 't':
+		switch unitStr[1] {
+		case 'b':
+			return TB, nil
+		case 'i':
+			return TiB, nil
+		case 'e':
+			switch unitStr[2] {
+			case 'b':
+				return TiB, nil
+			case 'r':
+				return TB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'p':
+		switch unitStr[1] {
+		case 'b':
+			return PB, nil
+		case 'i':
+			return PiB, nil
+		case 'e':
+			switch unitStr[2] {
+			case 'b':
+				return PiB, nil
+			case 't':
+				return PB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'e':
+		switch unitStr[1] {
+		case 'b':
+			return EB, nil
+		case 'i':
+			return EiB, nil
+		case 'x':
+			switch unitStr[2] {
+			case 'b':
+				return EiB, nil
+			case 'a':
+				return EB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'z':
+		switch unitStr[1] {
+		case 'b':
+			return ZB, nil
+		case 'i':
+			return ZiB, nil
+		case 'e':
+			switch unitStr[2] {
+			case 'b':
+				return ZiB, nil
+			case 't':
+				return ZB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'y':
+		switch unitStr[1] {
+		case 'b':
+			return YB, nil
+		case 'i':
+			return YiB, nil
+		case 'o':
+			switch unitStr[2] {
+			case 'b':
+				return YiB, nil
+			case 't':
+				return YB, nil
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'r':
+		switch unitStr[1] {
+		case 'b':
+			return RB, nil
+		case 'i':
+			return RiB, nil
+		case 'o':
+			switch unitStr[2] {
+			case 'b':
+				return RiB, nil
+			case 'n':
+				switch unitStr[3] {
+				case 'n':
+					switch unitStr[4] {
+					case 'i':
+						return RiB, nil
+					case 'a':
+						return RB, nil
+					default:
+						return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+					}
+				default:
+					return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+				}
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	case 'q':
+		switch unitStr[1] {
+		case 'b':
+			return QB, nil
+		case 'i':
+			return QiB, nil
+		case 'u':
+			switch unitStr[2] {
+			case 'e':
+				switch unitStr[3] {
+				case 't':
+					switch unitStr[4] {
+					case 't':
+						switch unitStr[5] {
+						case 'i':
+							return QiB, nil
+						case 'a':
+							return QB, nil
+						default:
+							return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+						}
+					default:
+						return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+					}
+				default:
+					return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+				}
+			default:
+				return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+			}
+		default:
+			return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+		}
+	default:
+		return Bytes{}, fmt.Errorf("unknown unit: %s", unitStr)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
 module github.com/beauhoyt/bytesize
 
-go 1.25.7
+go 1.26.0
 
 tool (
 	github.com/beauhoyt/bytesize
+	github.com/fzipp/gocyclo/cmd/gocyclo
 	golang.org/x/perf/cmd/benchstat
 )
 
 require (
 	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
+	github.com/fzipp/gocyclo v0.6.0 // indirect
 	golang.org/x/perf v0.0.0-20260112171951-5abaabe9f1bd // indirect
 )
